4950285e4d52d804f78d1937bc3b53934393fa08
RicePandaaaa <anthony.ha.pham@tamu.edu>
2023-12-19 08:28:27 -0600

    Edit border thickness

--- main-gui.py
+++ main-gui.py
@@ -1,14 +1,18 @@
 import tkinter as tk
-import clockframe
+import clockframe, motivation_message
 
 # Set up the screen
 screen = tk.Tk()
 screen.geometry("800x480")
 screen.title("PandaPi")
 
-# Create a clock
-current_time = clockframe.ClockFrame(master=screen)
-current_time.place(x=0, y=0)
+# Create a clock frame
+clock_frame = clockframe.ClockFrame(master=screen)
+clock_frame.place(x=0, y=0)
+
+# Create the motivational message frame
+message_frame = motivation_message.MotivationalMessageFrame(master=screen)
+message_frame.place(x=320, y=0)
 
 # Loop the screen
 screen.mainloop()
