540f0258a09b20c7d2b6cb9fb52188a7ce02fed8
RicePandaaaa <anthony.ha.pham@tamu.edu>
2023-12-19 05:03:36 -0600

    Add title, make screen fit LCD display dimensions

--- main-gui.py
+++ main-gui.py
@@ -3,7 +3,8 @@ import clockframe
 
 # Set up the screen
 screen = tk.Tk()
-screen.attributes('-fullscreen', True)
+screen.geometry("800x480")
+screen.title("PandaPi")
 
 # Create a clock
 current_time = clockframe.ClockFrame(master=screen)
